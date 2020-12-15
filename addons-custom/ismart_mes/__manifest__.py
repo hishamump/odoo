b295614e1e12266236d53d89d15f4f6e80c4d765
Hisham <49261241+hishamump@users.noreply.github.com>
2020-12-15 21:03:32 +0800

    Factory sequence
    Add the field factory_sequence

--- addons-custom/ismart_mes/__manifest__.py
+++ addons-custom/ismart_mes/__manifest__.py
@@ -17,6 +17,7 @@
     'depends': [],
     'data': [
         'security/ir.model.access.csv',
+        'data/sequence.xml',
         'views/workstation.xml',
         'views/factory.xml',
     ],
--- addons-custom/ismart_mes/models/factory.py
+++ addons-custom/ismart_mes/models/factory.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # Created by Hisham.
 
-from odoo import models, fields
+from odoo import models, fields, api, _
 
 
 class Factory(models.Model):
@@ -13,4 +13,12 @@ class Factory(models.Model):
     serial = fields.Char(string='Serial')
     address = fields.Char(string='address')
     notes = fields.Char(string='Notes')
+    factory_sequence = fields.Char(string='Reference', required=True, copy=False, readonly=True,
+                                   index=True, default=lambda self: _('New'))
 
+    @api.model
+    def create(self, vals):
+        if vals.get('factory_sequence', _('New')) == _('New'):
+            vals['factory_sequence'] = self.env['ir.sequence'].next_by_code('ismart.mes.factory.sequence') or _('New')
+        result = super(Factory, self).create(vals)
+        return result
