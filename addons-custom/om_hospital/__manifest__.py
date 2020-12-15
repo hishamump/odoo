cd5736291af79402bf2f4d02375e43135d1ca2a4
Hisham <49261241+hishamump@users.noreply.github.com>
2020-12-15 11:39:27 +0800

    Push om_hospital and test down in the menu
    Just to hide them from main menu display

--- addons-custom/om_hospital/__manifest__.py
+++ addons-custom/om_hospital/__manifest__.py
@@ -1,10 +1,11 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
+#        'security/ir.model.access.csv',
 {
     'name' : 'Hospial Management',
     'version' : '14.0.1.1',
     'summary': 'Module for managing a hospital',
-    'sequence': 2,
+    'sequence': 16,
     'description': """
         Test adding new addon
         ====================
@@ -16,7 +17,6 @@
     'images': [],
     'depends': [],
     'data': [
-        'security/ir.model.access.csv',
         'patient.xml',
     ],
     'installable': True,
--- addons-custom/test/__manifest__.py
+++ addons-custom/test/__manifest__.py
@@ -1,10 +1,11 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
+#        'security/ir.model.access.csv',
 {
     'name' : 'Test',
     'version' : '14.0.1.1',
     'summary': 'Test adding new addon',
-    'sequence': 1,
+    'sequence': 15,
     'description': """
         Test adding new addon
         ====================
@@ -16,7 +17,7 @@
     'images': [],
     'depends': [],
     'data': [
-        'security/ir.model.access.csv',
+
         'test.xml'
     ],
     'installable': True,
