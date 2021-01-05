017d16a36a21695dcc2bbc19544e1658cd42a906
Hisham <49261241+hishamump@users.noreply.github.com>
2021-01-05 16:32:57 +0800

    openacademy - Add Course & Session models

--- addons-custom/openacademy/__manifest__.py
+++ addons-custom/openacademy/__manifest__.py
@@ -3,7 +3,7 @@
     'name': "Open Academy",
 
     'summary': """Manage Training""",
-
+    'sequence': 16,
     'description': """
         Open Academy module for managing trainings:
             - training courses
@@ -11,13 +11,13 @@
             - attendees registration
     """,
 
-    'author': "My Company",
+    'author': "Hisham @ Academy",
     'website': "http://www.yourcompany.com",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
-    'category': 'Test',
+    'category': 'Extra Tools',
     'version': '0.1',
 
     # any module necessary for this one to work correctly
@@ -25,12 +25,16 @@
 
     # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
-        'views/views.xml',
-        'views/templates.xml',
+        'security/ir.model.access.csv',
+        # 'views/views.xml',
+        # 'views/templates.xml',
+        'views/openacademy.xml',
     ],
     # only loaded in demonstration mode
-    'demo': [
-        'demo/demo.xml',
-    ],
+    # 'demo': [
+    #     'demo/demo.xml',
+    # ],
+    'installable': True,
+    'application': True,
+    'auto_install': False
 }
