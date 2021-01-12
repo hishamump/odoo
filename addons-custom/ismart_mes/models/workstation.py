9c71db9cfa7137ae34d6b8340d319a85e1c246cd
Hisham <49261241+hishamump@users.noreply.github.com>
2021-01-12 23:29:05 +0800

    Create workstation reference

--- addons-custom/ismart_mes/models/workstation.py
+++ addons-custom/ismart_mes/models/workstation.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # Created by Hisham.
 
-from odoo import models, fields
+from odoo import models, fields, api, _
 
 
 class Workstation(models.Model):
@@ -9,8 +9,16 @@ class Workstation(models.Model):
     _description = 'Basic production unit'
     _rec_name = 'name'
 
+    workstation_sequence = fields.Char(string='Reference', required=True, copy=False, readonly=True,
+                                   index=True, default=lambda self: _('New'))
     name = fields.Char(string='Name')
-    serial = fields.Char(string='Serial')
     location = fields.Char(string='Location')
     notes = fields.Char(string='Notes')
     image = fields.Binary('Image')
+
+    @api.model
+    def create(self, values):
+        if values.get('workstation_sequence', _('New')) == _('New'):
+            values['workstation_sequence'] = self.env['ir.sequence'].next_by_code('ismart.mes.workstation.sequence') or _('New')
+        result = super(Workstation, self).create(values)
+        return result
