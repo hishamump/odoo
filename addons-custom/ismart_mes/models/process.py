f72d3545624a1d205869f2840071cc844923440b
Hisham <49261241+hishamump@users.noreply.github.com>
2021-01-12 17:12:47 +0800

    Create process tree view

--- addons-custom/ismart_mes/models/process.py
+++ addons-custom/ismart_mes/models/process.py
@@ -7,3 +7,15 @@ from odoo import models, fields
 class Process(models.Model):
     _name = 'process'
     _description = 'Production Processes'
+
+    name = fields.Char(string='Name')
+    start_date = fields.Datetime(string='Start Date')
+    end_date = fields.Datetime(string='End Date')
+    order_id = fields.Many2one('mrp.production',
+                               ondelete='set null', string="Order", index=True)
+
+    def start(self):
+        print("Started")
+
+    def stop(self):
+        print("Started")
