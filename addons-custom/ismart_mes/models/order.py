2b570321adfbe4a4b6ecf0e94238632f84d08113
Hisham <49261241+hishamump@users.noreply.github.com>
2021-01-15 00:59:32 +0800

    Button & list create process

--- addons-custom/ismart_mes/models/order.py
+++ addons-custom/ismart_mes/models/order.py
@@ -1,8 +1,23 @@
 # -*- coding: utf-8 -*-
 # Created by Hisham.
 
-from odoo import models, fields
+from odoo import models, fields, _
 
 
 class Order(models.Model):
     _inherit = 'mrp.production'
+
+    process_id = fields.One2many('process', 'order_id', string="Processes")
+
+    def create_process(self):
+        return {
+            'name': _('Process'),
+            'domain': [],
+            'view_type': 'form',
+            'res_model': 'process',
+            'view_id': False,
+            'view_mode': 'form',
+            'type': 'ir.actions.act_window',
+            # TODO: This id order_id doesnt passed successfully, need more read
+            'order_id': self._context.get('order_id', False)
+        }
