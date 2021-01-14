5d486dc653746d542af75be07578717eca785b02
Hisham <49261241+hishamump@users.noreply.github.com>
2021-01-14 17:57:15 +0800

    Fix order model and view

--- addons-custom/ismart_mes/models/order.py
+++ addons-custom/ismart_mes/models/order.py
@@ -6,7 +6,3 @@ from odoo import models, fields
 
 class Order(models.Model):
     _inherit = 'mrp.production'
-    _name = 'order'
-    _description = 'Production Orders'
-
-    name = fields.Char("Process", required=True)
